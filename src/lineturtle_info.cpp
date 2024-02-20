c7a8b537703c4a107039333ad46f2328af2df477
AdithyaDamarla <adithyaprakash12@gmail.com>
2024-02-20 15:57:22 +0100

    Project completed

---
+++ src/lineturtle_info.cpp
@@ -0,0 +1,29 @@
+#include <cstdio>
+
+int main(int argc, char ** argv)
+{
+  (void) argc;
+  (void) argv;
+
+  printf("hello world lineturtle package\n");
+    printf("Introducing \"Lineturtle\" by RoboFuntastic: Unleash Creativity with the Ultimate Drawing Robot!\n\n");
+
+    
+    printf("Key Features:\n");
+    printf("1. Easy Assembly for All Ages: Lineturtle comes with CAD files for 3D printing, circuit files for electronics, and meticulously crafted step-by-step instructions. Build your Lineturtle effortlessly and embark on a journey of discovery.\n");
+    printf("2. Versatile Servo-Mounted Marker: Lineturtle's back servo allows you to mount a marker, giving you precise control over touching the floor or lifting it. Create intricate designs or simply doodle – the possibilities are endless.\n");
+    printf("3. Joyful Control via Joystick: Use the included joystick to interact with Lineturtle and watch your commands come to life. Feel the thrill of controlling every movement, turning your ideas into reality with the tip of your fingers.\n");
+    printf("4. Seamless ROS2 Integration: Lineturtle communicates via ROS2 topics, making it an ideal platform for budding robotic enthusiasts. Topics like /servo, /LEDs, /left_motor_ticks, /right_motor_ticks, /battery, and /cmd_vel empower you with complete control and insight into Lineturtle's actions.\n");
+    printf("5. Dazzling LEDs: Lineturtle features two LEDs mounted in front, adding a touch of brilliance to your creations. Illuminate your path and paint the night with vibrant colors.\n");
+    printf("6. Educational and Fun: Lineturtle is designed to be a gateway to the world of ROS2 for absolute beginners. Learn the ropes quickly, experiment with drawing, and get creative! It's the perfect blend of education and enjoyment.\n");
+    printf("7. Long-lasting Rechargeable Battery: Lineturtle is built to last. The rechargeable battery provides hours of uninterrupted playtime, ensuring your creativity flows without interruption.\n");
+    
+    printf("Get Started Today:\n");
+    printf("1. Discover the joy of robotic control through the joystick interface.\n");
+    printf("2. Explore ROS2 topics and learn the basics of robotics.\n");
+    printf("3. Unleash your creativity with Lineturtle's drawing capabilities.\n");
+    printf("4. Follow RoboFuntastic for continuous learning and inspiration.\n");
+    printf("5. Recharge, reimagine, and repeat – Lineturtle is your partner in exploration.\n\n");
+
+  return 0;
+}
